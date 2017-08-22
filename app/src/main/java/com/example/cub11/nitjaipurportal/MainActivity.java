3ef18535b66c5a9ee20d04d5df9d637cd078d614
mamta limba <mamta.limba@cubastion.com>
2017-08-22 15:30:29 +0530

    commit

---
+++ app/src/main/java/com/example/cub11/nitjaipurportal/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.cub11.nitjaipurportal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_login, btn_signup;
+    EditText et_username, et_password;
+    String username, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_new_activity);
+
+        btn_login=(Button)findViewById(R.id.button);
+        btn_signup=(Button)findViewById(R.id.button2);
+
+        et_username=(EditText)findViewById(R.id.editText_username);
+        et_password=(EditText)findViewById(R.id.editText_password);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username=et_username.getText().toString();
+            }
+        });
+
+
+
+    }
+
+
+
+}
